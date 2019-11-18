33f583baecb495f200159d4bd2abf33ea907aeb1
tina0405 <tina423424@gmail.com>
2019-11-18 12:46:28 +0800

    Add a config file in operation file.

--- E.file_system/testcode/GPIO/GPIO.c
+++ E.file_system/testcode/GPIO/GPIO.c
@@ -11,26 +11,42 @@ void DELAY(unsigned long def,int on_off){
 	);
 
 }
-
-void init_compt(void){ /*initial*/
-	kservice_uart_write("Initial GPIO component!\n\r");
-	kservice_uart_write("%d\n\r",A(5));
-	delay(5);
-	kservice_reg_compt("set_gpio");
-}
-
+struct para{
+	int para_num;
+	unsigned long gpio;
+	int on_off;
+};
+struct para input;
+struct para drv_config =
+ {
+    para_num: 2,
+    gpio: sizeof(input.gpio),
+    on_off: sizeof(input.on_off),
+ };
 
 
 int A(int a){
 	return a+1;
 }
 
+void init_compt(void){ /*initial*/
+	kservice_uart_write("Initial GPIO component!\n\r");
+	kservice_uart_write("%d\n\r",sizeof(input.gpio));
+	kservice_uart_write("%d\n\r",sizeof(input.on_off));
+	kservice_uart_write("%d\n\r",A(5));
+	delay(5);
+	//kservice_config_compt(drv_config);
+	kservice_reg_compt("set_gpio");
+}
 
-void oprt_compt(unsigned long gpio,int on_off){ /*operation*/
+
+
+
+void oprt_compt(struct para parameter){ /*operation*/
 	kservice_uart_write("GPIO Operation!\n\r");  	
-	kservice_put32(GPPUD,on_off);
+	kservice_put32(GPPUD,parameter.on_off);
         delay(150);
-        kservice_put32(GPPUDCLK0,(1<<gpio));
+        kservice_put32(GPPUDCLK0,(1<<parameter.gpio));
         delay(150);
         kservice_put32(GPPUDCLK0,0);      
 }
